88d84cd2ed76cd5b805c1e31a01171e4d28a2105
Dzhelil Rufat <drufat@caltech.edu>
2017-02-13 09:32:40 -0800

    Add int dtype.

--- include/pybindcpp/ctypes.h
+++ include/pybindcpp/ctypes.h
@@ -36,12 +36,14 @@ const std::map<std::type_index, const char *> ctype_map = {
     {typeid(wchar_t *), "c_wchar_p"},
     {typeid(void *), "c_void_p"},
     {typeid(PyObject *), "py_object"},
+    {typeid(int *), "POINTER(c_int)"},
     {typeid(double *), "POINTER(c_double)"},
 
     {typeid(const char *), "c_char_p"},
     {typeid(const wchar_t *), "c_wchar_p"},
     {typeid(const void *), "c_void_p"},
     {typeid(const PyObject *), "py_object"},
+    {typeid(const int *), "POINTER(c_int)"},
     {typeid(const double *), "POINTER(c_double)"},
 
     {typeid(void), "None"},
